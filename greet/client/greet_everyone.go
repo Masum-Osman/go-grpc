221c4df1039ebfb49f17f71f36937b76ec7dea37
Masum_Osman <ogkmasum@gmail.com>
2023-09-11 19:50:56 +0600

    bi-directional was done. but problem with go routine exist

---
+++ greet/client/greet_everyone.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	pb "go-grpc/greet/proto"
+	"io"
+	"log"
+	"time"
+)
+
+func doGreetEveryone(c pb.GreetServiceClient) {
+	log.Println("doGreetEveryone was invoked")
+
+	stream, err := c.GreetEveryone(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error while creating stream: %v\n", err)
+	}
+
+	reqs := []*pb.GreetRequest{
+		{FirstName: "Masum"},
+		{FirstName: "Mishqat"},
+		{FirstName: "Khan"},
+		{FirstName: "Osman"},
+		{FirstName: "Jahan"},
+	}
+
+	waitc := make(chan struct{})
+
+	go func() {
+		for _, req := range reqs {
+			log.Printf("Sending request: %v\n", req)
+			stream.Send(req)
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Printf("Error while receiving stream: %v\n", err)
+				break
+			}
+
+			log.Printf("Received: %v\n", res.Result)
+		}
+
+		close(waitc)
+	}()
+
+	<-waitc
+}
